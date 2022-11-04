634fcb927f030d310926407af7aa75a4f8f8a2f8
Eduardo Borsa <borsa.dado@gmail.com>
2022-11-04 10:53:32 -0300

    tutorial live already created

---
+++ lib/tutorial_web/live/tutorial_live/index.ex
@@ -0,0 +1,9 @@
+defmodule TutorialWeb.TutorialLive.Index do
+  use TutorialWeb, :live_view
+
+  @impl true
+  def mount(_assigns, _session, socket) do
+    
+    {:ok, socket}
+  end
+end
--- lib/tutorial_web/router.ex
+++ lib/tutorial_web/router.ex
@@ -18,6 +18,7 @@ defmodule TutorialWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    live "/tutorial", TutorialLive.Index, :index
   end
 
   # Other scopes may use custom stacks.
